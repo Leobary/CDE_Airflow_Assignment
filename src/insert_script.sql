9679aa8673a54d446353ef63ae6636e821d0e00a
Leobary <leobary86@yahoo.com>
2024-10-19 13:39:36 +0100

    my first commit

---
+++ src/insert_script.sql
@@ -0,0 +1,5 @@
+INSERT INTO sentiments VALUES ('Amazon', 371, NOW());
+INSERT INTO sentiments VALUES ('Apple', 473, NOW());
+INSERT INTO sentiments VALUES ('Facebook', 2477, NOW());
+INSERT INTO sentiments VALUES ('Google', 1743, NOW());
+INSERT INTO sentiments VALUES ('Microsoft', 726, NOW());
